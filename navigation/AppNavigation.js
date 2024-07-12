c6520f19a0de81ff9adb75cfc412460848109711
Bilal Haider <haiderbilal306@gmail.com>
2024-07-11 23:56:26 -0700

    Project Setup

---
+++ navigation/AppNavigation.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from '../screens/HomeScreen'
+const AppNavigation = () => {
+  const Stack= createNativeStackNavigator
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default AppNavigation
